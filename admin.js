e068c74fdbadeee02d0b4cc5d6f862f2cb6fbcad
ltlist <47967967+ltlist@users.noreply.github.com>
2026-06-04 21:50:14 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,52 +1,44 @@
-let data = JSON.parse(localStorage.getItem("admin_data") || "[]");
+let data=JSON.parse(localStorage.getItem("adm")||"[]");
 
-const list = document.getElementById("list");
+const list=document.getElementById("list");
 
-// render
-function render() {
-    list.innerHTML = "";
-
-    data.forEach((f, i) => {
-        list.innerHTML += `
-        <tr>
-            <td>${f.nama}</td>
-            <td>${f.koin}</td>
-            <td>
-                <button onclick="hapus(${i})">❌</button>
-            </td>
-        </tr>
-        `;
-    });
-
-    localStorage.setItem("admin_data", JSON.stringify(data));
+function render(){
+list.innerHTML="";
+data.forEach((x,i)=>{
+list.innerHTML+=`
+<tr>
+<td>${x.nama}</td>
+<td>${x.koin}</td>
+<td><button onclick="del(${i})">Hapus</button></td>
+</tr>`;
+});
+localStorage.setItem("adm",JSON.stringify(data));
 }
 
-function addFaucet() {
-    data.push({
-        id: Date.now().toString(),
-        nama: nama.value,
-        koin: koin.value,
-        trust: trust.value,
-        reward: reward.value,
-        link: link.value,
-        cooldown: parseInt(cooldown.value)
-    });
-
-    render();
+function add(){
+data.push({
+id:Date.now().toString(),
+nama:nama.value,
+koin:koin.value,
+trust:trust.value,
+reward:reward.value,
+link:link.value,
+cooldown:Number(cooldown.value)
+});
+render();
 }
 
-function hapus(i) {
-    data.splice(i, 1);
-    render();
+function del(i){
+data.splice(i,1);
+render();
 }
 
-// export JSON untuk GitHub
-function exportJSON() {
-    const blob = new Blob([JSON.stringify(data, null, 2)], {type:"application/json"});
-    const a = document.createElement("a");
-    a.href = URL.createObjectURL(blob);
-    a.download = "faucet.json";
-    a.click();
+function save(){
+let blob=new Blob([JSON.stringify(data,null,2)],{type:"application/json"});
+let a=document.createElement("a");
+a.href=URL.createObjectURL(blob);
+a.download="faucet.json";
+a.click();
 }
 
 render();
