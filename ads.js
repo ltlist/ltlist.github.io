1cec04c8bad46a958c79746045d68d9d259242de
ltlist <47967967+ltlist@users.noreply.github.com>
2026-06-09 06:17:33 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -2,46 +2,71 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.querySelectorAll(".ad-slot").forEach(el => {
 
-    const type = el.getAttribute("data-type");
+    const network = el.getAttribute("data-network");
+
+    let iframe = "";
 
     // =========================
     // ZERADS
     // =========================
-    if (type === "zerads") {
-      el.innerHTML = `
-        <div id="zerads-container"></div>
-        <script src="https://zerads.com/ad.js"></script>
+    if (network === "zerads") {
+      iframe = `
+        <iframe
+          src="https://zerads.com/ad/ad.php?width=468&ref=11338"
+          marginwidth="0"
+          marginheight="0"
+          width="468"
+          height="60"
+          scrolling="no"
+          frameborder="0"
+          style="border:0;overflow:hidden;">
+        </iframe>
       `;
     }
 
     // =========================
     // A-ADS
     // =========================
-    else if (type === "aads") {
-      el.innerHTML = `
-        <!-- A-ADS -->
-        <ins class="a-ads"
-          data-ad-client="YOUR_ID"
-          data-ad-slot="YOUR_SLOT"
-          style="display:block"></ins>
-        <script src="https://a-ads.com/js/ads.js"></script>
-        <script>
-          if(window.aads) aads.init();
-        </script>
+    else if (network === "aads") {
+      iframe = `
+        <iframe
+          src="https://a-ads.com/your-ad-link"
+          width="300"
+          height="250"
+          scrolling="no"
+          frameborder="0"
+          marginwidth="0"
+          marginheight="0"
+          style="border:0;overflow:hidden;">
+        </iframe>
       `;
     }
 
     // =========================
     // FAUCETPAY ADS
     // =========================
-    else if (type === "faucetpay") {
-      el.innerHTML = `
+    else if (network === "faucetpay") {
+      iframe = `
         <iframe
           src="https://faucetpay.io/advertise"
-          style="width:100%;height:60px;border:0;overflow:hidden;">
+          width="468"
+          height="60"
+          scrolling="no"
+          frameborder="0"
+          style="border:0;overflow:hidden;">
         </iframe>
       `;
     }
+
+    // INSERT INTO PAGE
+    el.innerHTML = `
+      <div class="ad-banner">
+        <div class="ad-label">Advertisement</div>
+        <div class="ad-box">
+          ${iframe}
+        </div>
+      </div>
+    `;
 
   });
 
