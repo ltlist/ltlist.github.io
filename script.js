902cd19e1869b897d63b174301c09b832a8c5cc6
ltlist <47967967+ltlist@users.noreply.github.com>
2026-06-09 10:35:51 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyAVokWj_l3aITEhj6UPetF-MGQXKdv75S8",
+  authDomain: "ltlist-f.firebaseapp.com",
+  projectId: "ltlist-f",
+  storageBucket: "ltlist-f.firebasestorage.app",
+  messagingSenderId: "991011425656",
+  appId: "1:991011425656:web:d8f4da4e5c4b4ab9aacc8d"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const listDiv = document.getElementById("list");
+
+// 🔥 LOAD FAUCETS (READ ONLY)
+async function loadFaucets(){
+  const snap = await getDocs(collection(db, "faucets"));
+
+  let html = "";
+
+  snap.forEach((doc) => {
+    const d = doc.data();
+
+    html += `
+      <div>
+        <b>${d.name}</b><br>
+        Coin: ${d.coin}<br><br>
+        <a href="${d.url}" target="_blank">Visit</a>
+      </div>
+    `;
+  });
+
+  listDiv.innerHTML = html;
+}
+
+loadFaucets();
