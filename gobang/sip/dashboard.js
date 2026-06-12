6af543755f50e204e2ab7c8f9f95042921a481ec
ltlist <47967967+ltlist@users.noreply.github.com>
2026-06-12 08:17:49 +0700

    Update dashboard.js

--- gobang/sip/dashboard.js
+++ gobang/sip/dashboard.js
@@ -1,6 +1,3 @@
-/* =========================
-   FIREBASE IMPORT
-========================= */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
 import {
