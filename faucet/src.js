2920960f8c07d2882a43a0c4c543f9b2aeff883d
ltlist <47967967+ltlist@users.noreply.github.com>
2026-06-16 21:04:33 +0700

    Update src.js

--- faucet/src.js
+++ faucet/src.js
@@ -1,32 +1,31 @@
-// first number is number of seconds
-  document.getElementById('click').innerHTML = "<b><p>↓ Click on the ad to Claim ↓</p></b>";
-  var tim3ba75fd550d12b36f0c4a1e8f808f74a6aee881ef65da32b98573e9fd7a76ad1 = 7;
-  var monitor = setInterval(function(){
-  var elem = document.activeElement;
-  if(elem && elem.tagName == 'IFRAME'){
-  document.getElementById('click').innerHTML = "<b><p>↓ Click on the ad to Claim ↓</p></b>";
-  clearInterval(monitor);
-  str3ba75fd550d12b36f0c4a1e8f808f74a6aee881ef65da32b98573e9fd7a76ad1();
-  }
-  },100);
-  function str3ba75fd550d12b36f0c4a1e8f808f74a6aee881ef65da32b98573e9fd7a76ad1() {
-  setInterval(function() {
-    if(tim3ba75fd550d12b36f0c4a1e8f808f74a6aee881ef65da32b98573e9fd7a76ad1 <= 0){
-        clearInterval(tim3ba75fd550d12b36f0c4a1e8f808f74a6aee881ef65da32b98573e9fd7a76ad1);
-        document.getElementById("click").innerHTML = "<b><p>Ad Click Completed. Thank You!</p></b>";
-        document.title = "Thanks!";
-        document.getElementById('3ba75fd550d12b36f0c4a1e8f808f74a6aee881ef65da32b98573e9fd7a76ad1').style.display = "block";
-        document.getElementById('s3ba75fd550d12b36f0c4a1e8f808f74a6aee881ef65da32b98573e9fd7a76ad1').style.display = "block";
+// adblock.js
+
+window.addEventListener("load", function () {
+
+  const warning = document.getElementById("block-adb-enabled");
+  const main = document.getElementById("main");
+
+  const checkAdblock = () => {
+
+    if (
+      document.getElementById("ToZQWzAfsUC") &&
+      document.getElementById("HoZQWzAfsUCj")
+    ) {
+
+      if (main) main.style.display = "block";
+      if (warning) warning.style.display = "none";
+
     } else {
-        timm = tim3ba75fd550d12b36f0c4a1e8f808f74a6aee881ef65da32b98573e9fd7a76ad1;
-        document.getElementById("click").innerHTML = "<b><p>Keep the ad open for " + timm + " more seconds to continue.</p></b>";
-        if(document.hasFocus()==false){
-        document.title = timm + " seconds left";
-        tim3ba75fd550d12b36f0c4a1e8f808f74a6aee881ef65da32b98573e9fd7a76ad1 -= 1;
-        } else {
-        document.title = "You are not viewing the ad!";
-        document.getElementById("click").innerHTML = "<b><p>You must click on the ad to continue!</p></b>";
-        }
+
+      if (warning) warning.style.display = "block";
+      if (main) main.style.display = "none";
+
+      alert("Disable AdBlock to claim rewards!");
+
     }
-  }, 1000);
-  }
+
+  };
+
+  setTimeout(checkAdblock, 3000);
+
+});
