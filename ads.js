55c0609b874a2d20a0c9c4c3684b809f7436101a
ltlist <47967967+ltlist@users.noreply.github.com>
2026-07-04 06:01:47 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,56 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // Load Surfe script sekali saja
-  if (!document.querySelector('script[src="//static.surfe.pro/js/net.js"]')) {
-    const s = document.createElement("script");
-    s.src = "//static.surfe.pro/js/net.js";
-    document.head.appendChild(s);
-  }
+document.querySelectorAll(".ad-slot").forEach(el => {
 
-  document.querySelectorAll(".ad-slot").forEach(el => {
+const network = el.dataset.network;  
+let iframe = "";  
 
-    const network = el.dataset.network;
-    let html = "";
+if (network === "zerads-468") {  
+  iframe = `  
+    <iframe  
+      class="ad-frame"  
+      src="https://zerads.com/ad/ad.php?width=468&ref=11448"  
+      width="468"  
+      height="60"  
+      frameborder="0"  
+      scrolling="no">  
+    </iframe>  
+  `;  
+}  
 
-    if (network === "zerads-468") {
-      html = `
-        <iframe
-          class="ad-frame"
-          src="https://zerads.com/ad/ad.php?width=468&ref=11448"
-          width="468"
-          height="60"
-          frameborder="0"
-          scrolling="no">
-        </iframe>
-      `;
-    }
+else if (network === "zerads-300") {  
+  iframe = `  
+    <iframe  
+      class="ad-frame"  
+      src="https://zerads.com/ad/ad.php?width=300&ref=11448"  
+      width="300"  
+      height="250"  
+      frameborder="0"  
+      scrolling="no">  
+    </iframe>  
+  `;  
+}  
 
-    else if (network === "zerads-300") {
-      html = `
-        <iframe
-          class="ad-frame"
-          src="https://zerads.com/ad/ad.php?width=300&ref=11448"
-          width="300"
-          height="250"
-          frameborder="0"
-          scrolling="no">
-        </iframe>
-      `;
-    }
+el.innerHTML = iframe;
 
-    else if (network === "surfe") {
-      html = `
-        <ins class="surfe-be" data-sid="421402"></ins>
-      `;
-    }
-
-    el.innerHTML = html;
-
-    // Jalankan Surfe setelah elemen dibuat
-    if (network === "surfe" && window.adsurfebe) {
-      window.adsurfebe.push({});
-    }
-
-  });
+});
 
 });
